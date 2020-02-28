b4fffdebfe5ae265d8abe83dd64dc14da20f76f6
obespsverde <sverde@autentia.com>
2020-02-28 08:59:37 +0100

    Refactor

--- src/main/java/com/sergio/dto/SimpleJavaTranslator.java
+++ src/main/java/com/sergio/dto/SimpleJavaTranslator.java
@@ -6,7 +6,6 @@ import com.sun.source.tree.VariableTree;
 import com.sun.tools.javac.code.Flags;
 import com.sun.tools.javac.code.Symtab;
 import com.sun.tools.javac.code.TypeTag;
-import com.sun.tools.javac.code.Types;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.TreeMaker;
 import com.sun.tools.javac.tree.TreeTranslator;
@@ -38,15 +37,18 @@ public class SimpleJavaTranslator extends TreeTranslator {
         final boolean isAnnotated = isClassAnnotated(clazz);
 
         if (isAnnotated) {
-            final com.sun.tools.javac.util.List<JCTree> getters = createGetters(context, clazz);
-            final com.sun.tools.javac.util.List<JCTree> setters = createSetters(context, clazz);
+
+            final TreeMaker maker = TreeMaker.instance(context);
+            final Names names = Names.instance(context);
+            final Symtab symb = Symtab.instance(context);
             
-            clazz.defs = clazz.defs
-                    .appendList(getters)
-                    .appendList(setters);
+            final com.sun.tools.javac.util.List<JCTree> getters = createGetters(maker, names, clazz);
+            final com.sun.tools.javac.util.List<JCTree> setters = createSetters(maker, names, symb, clazz);
+            
+            clazz.defs = clazz.defs.appendList(getters).appendList(setters);
             result = clazz;
 
-            System.out.println("result = " + result);
+            System.out.println("class after translate = " + result);
         }
 
     }
@@ -56,29 +58,25 @@ public class SimpleJavaTranslator extends TreeTranslator {
      *  return <member>;
      * }
      */
-    private com.sun.tools.javac.util.List<JCTree> createGetters(Context context, ClassTree node) {
-        
-        final Names names = Names.instance(context);
-        final TreeMaker maker = TreeMaker.instance(context);
-
+    private com.sun.tools.javac.util.List<JCTree> createGetters(TreeMaker maker, Names names, ClassTree node) {
         return com.sun.tools.javac.util.List.from(node.getMembers().stream()
                 .filter(m -> m.getKind() == Tree.Kind.VARIABLE)
                 .map(m -> {
                     final VariableTree varTree = (VariableTree) m;
+                    final Tree type = varTree.getType();
                     final Name name = names.fromString(varTree.getName().toString());
                     final Name methodName = names.fromString("get" + capitalize(name));
                     final JCTree.JCExpression memberName = maker.Ident(name);
-                    final Tree type = varTree.getType();
                     final JCTree.JCBlock block = maker.Block(0, List.of(maker.Return(memberName)));
                     return maker.MethodDef(
-                            maker.Modifiers(Flags.PUBLIC),
-                            methodName,
-                            calculateReturnType(type),
-                            List.nil(), // generic type parameters
-                            List.nil(), // parameter list
-                            List.nil(), // throws clause
-                            block,
-                            null
+                            maker.Modifiers(Flags.PUBLIC),  // method access modifier
+                            methodName,                     // method name
+                            generateReturnType(type),       // return type
+                            List.nil(),                     // generic type parameters
+                            List.nil(),                     // parameter list
+                            List.nil(),                     // throws clause
+                            block,                          // method body
+                            null                // default methods (for interface declaration)
                     ).getTree();
                 }).collect(Collectors.toList()));
     }
@@ -88,13 +86,7 @@ public class SimpleJavaTranslator extends TreeTranslator {
      *   this.<member> = <MemberName>;
      * }
      */
-    private com.sun.tools.javac.util.List<JCTree> createSetters(Context context, ClassTree node) {
-
-        final Names names = Names.instance(context);
-        final TreeMaker maker = TreeMaker.instance(context);
-        final Symtab symb = Symtab.instance(context);
-        final Types types = Types.instance(context);
-
+    private com.sun.tools.javac.util.List<JCTree> createSetters(TreeMaker maker, Names names, Symtab symb, ClassTree node) {
         return com.sun.tools.javac.util.List.from(node.getMembers().stream()
                 .filter(m -> m.getKind() == Tree.Kind.VARIABLE)
                 .map(m -> {
@@ -103,31 +95,30 @@ public class SimpleJavaTranslator extends TreeTranslator {
                     final Name name = names.fromString(varTree.getName().toString());
                     final JCTree.JCExpression memberName = maker.Ident(name);
 
-                    final String capitalizedParameterName = "a" + capitalize(varTree.getName().toString());
-                    final Name parameterName = names.fromString(capitalizedParameterName);
-
-                    final JCTree.JCVariableDecl param = calculateParam(parameterName, maker, varTree, symb);
+                    final JCTree.JCVariableDecl param = generateParameter(varTree, maker, names, symb);
                     final JCTree.JCAssign assign = maker.Assign(memberName, maker.Ident(param.getName()));
                     final JCTree.JCExpressionStatement exec = maker.Exec(assign);
                     final JCTree.JCBlock block = maker.Block(0, List.of(exec));
                     
                     return maker.MethodDef(
-                            maker.Modifiers(Flags.PUBLIC),
-                            methodName,
-                            maker.TypeIdent(TypeTag.VOID),
-                            List.nil(),     // generic type parameters
-                            List.of(param), // parameter list
-                            List.nil(),     // throws clause
-                            block,          // method body
-                            null    // default methods (for interface declaration)    
+                            maker.Modifiers(Flags.PUBLIC),  // method access modifier
+                            methodName,                     // method name
+                            maker.TypeIdent(TypeTag.VOID),  // return type
+                            List.nil(),                     // generic type parameters
+                            List.of(param),                 // parameter list
+                            List.nil(),                     // throws clause
+                            block,                          // method body
+                            null                // default methods (for interface declaration)    
                     ).getTree();
                 }).collect(Collectors.toList()));
     }
 
-    private JCTree.JCVariableDecl calculateParam(Name parameterName, TreeMaker maker, VariableTree varTree, Symtab syms) {
+    private JCTree.JCVariableDecl generateParameter(VariableTree varTree, TreeMaker maker, Names names, Symtab syms) {
+        final String capitalizedParameterName = "a" + capitalize(varTree.getName().toString());
+        final Name parameterName = names.fromString(capitalizedParameterName);
         if (varTree.getType().getClass().equals(JCTree.JCIdent.class)) {
             JCTree.JCIdent type = (JCTree.JCIdent) varTree.getType();
-            return maker.at(type.pos).Param(parameterName, type.type, null);
+            return maker.at(type.pos).Param(parameterName, type.type, syms.noSymbol);
         } else if (varTree.getType().getClass().equals(JCTree.JCPrimitiveTypeTree.class)) {
             JCTree.JCPrimitiveTypeTree type = (JCTree.JCPrimitiveTypeTree) varTree.getType();
             return maker.at(type.pos).Param(parameterName, type.type, syms.noSymbol);
@@ -135,7 +126,7 @@ public class SimpleJavaTranslator extends TreeTranslator {
             JCTree.JCArrayTypeTree type = (JCTree.JCArrayTypeTree) varTree.getType();
             return maker.at(type.pos).Param(parameterName, type.type, syms.noSymbol);
         } else {
-            throw new IllegalStateException("type not found = " + varTree.getType().getClass());
+            throw new IllegalStateException("type not detected = " + varTree.getType().getClass());
         }
     }
 
@@ -143,7 +134,7 @@ public class SimpleJavaTranslator extends TreeTranslator {
         return (char) (name.charAt(0) ^ ' ') + name.toString().substring(1);
     }
 
-    private JCTree.JCExpression calculateReturnType(Tree type) {
+    private JCTree.JCExpression generateReturnType(Tree type) {
         if (type.getClass().equals(JCTree.JCPrimitiveTypeTree.class)) {
             return (JCTree.JCPrimitiveTypeTree) type;
         } else if (JCTree.JCIdent.class.equals(type.getClass())){
